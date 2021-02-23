3a47b332bab9d9cf062e898a7590e4e6c6f43186
lunnik9 <AKatayev@one.kz>
2021-02-24 02:56:02 +0600

    feat: server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello heroku")
+	port := os.Getenv("PORT")
+	fmt.Println("Listening on", port)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	})
+	http.ListenAndServe(":"+port, nil)
 }
